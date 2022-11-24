8dfef3d784be8367391b33ffd700b96044149468
Igor <88189374+igor399@users.noreply.github.com>
2022-11-24 20:48:48 +0300

    IoC pt.1, pt.2

---
+++ src/spring_intro/Cat.java
@@ -0,0 +1,8 @@
+package spring_intro;
+
+public class Cat implements Pet {
+    @Override
+    public void say() {
+        System.out.println("Meow-mew");
+    }
+}
---
+++ src/spring_intro/Test1.java
@@ -0,0 +1,9 @@
+package spring_intro;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Pet pet = new Cat();
+        pet.say();
+
+    }
+}
