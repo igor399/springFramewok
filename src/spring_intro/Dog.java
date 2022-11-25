b337bc4f3f030c9b117866e49cebb2789d342096
Igor <88189374+igor399@users.noreply.github.com>
2022-11-25 17:46:01 +0300

    singleton and prototype beans

--- src/spring_intro/Dog.java
+++ src/spring_intro/Dog.java
@@ -1,6 +1,8 @@
 package spring_intro;
 
 public class Dog implements Pet {
+    private String name;
+
     public Dog() {
         System.out.println(">>>dog constr created<<<");
     }
@@ -8,5 +10,13 @@ public class Dog implements Pet {
     @Override
     public void say() {
         System.out.println("Bow-wow");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
