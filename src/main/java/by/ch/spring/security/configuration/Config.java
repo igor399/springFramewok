d13d8d83c71695e1d2be7fe3939669f820866040
Igor <88189374+igor399@users.noreply.github.com>
2022-12-07 20:22:15 +0300

    bcrypt

--- src/main/java/by/ch/spring/security/configuration/Config.java
+++ src/main/java/by/ch/spring/security/configuration/Config.java
@@ -1,6 +1,8 @@
 package by.ch.spring.security.configuration;
 
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,8 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 
 @Configuration
@@ -21,5 +25,19 @@ public class Config {
         internalResourceViewResolver.setPrefix("WEB-INF/view/");
         internalResourceViewResolver.setSuffix(".jsp");
         return internalResourceViewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&serverTimezone=UTC");
+            dataSource.setUser("bestuser");
+            dataSource.setPassword("bestuser");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
     }
 }
--- src/main/java/by/ch/spring/security/configuration/SecurityConfig.java
+++ src/main/java/by/ch/spring/security/configuration/SecurityConfig.java
@@ -1,5 +1,6 @@
 package by.ch.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,15 +8,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("igor").password("igor").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("cyber").password("cyber").roles("HR"))
-                .withUser(userBuilder.username("dogecoin").password("dogecoin").roles("IT"));
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder.username("igor").password("igor").roles("EMPLOYEE"))
+//                .withUser(userBuilder.username("cyber").password("cyber").roles("HR"))
+//                .withUser(userBuilder.username("dogecoin").password("dogecoin").roles("IT"));
+
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Override
