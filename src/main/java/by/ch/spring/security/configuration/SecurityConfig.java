bc9ae92db572ad33e2e02d976bb4b0683124df9f
Igor <88189374+igor399@users.noreply.github.com>
2022-12-07 19:31:31 +0300

    Authorization procedure

--- src/main/java/by/ch/spring/security/configuration/SecurityConfig.java
+++ src/main/java/by/ch/spring/security/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package by.ch.spring.security.configuration;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -15,5 +16,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(userBuilder.username("igor").password("igor").roles("EMPLOYEE"))
                 .withUser(userBuilder.username("cyber").password("cyber").roles("HR"))
                 .withUser(userBuilder.username("dogecoin").password("dogecoin").roles("IT"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "IT")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manager_info").hasRole("IT")
+                .and().formLogin().permitAll();
     }
 }
--- src/main/webapp/WEB-INF/view/allEmployees.jsp
+++ src/main/webapp/WEB-INF/view/allEmployees.jsp
@@ -1,14 +1,19 @@
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 <!DOCTYPE html>
 <html>
 <body>
 <h2>inf for all employees</h2>
 <br><br>
+<security:authorize access="hasRole('HR')">
 <input type="button" value="salary" onclick="window.location.href='hr_info'">
 <br>
 only for HR
+</security:authorize>
 <br><br>
+<security:authorize access="hasRole('IT')">
 <input type="button" value="performance" onclick="window.location.href='manager_info'">
 <br>
 only for managers
+</security:authorize>
 </body>
 </html>
