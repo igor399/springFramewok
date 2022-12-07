9f349d2e83a077511302adb4ea97be869493eb27
Igor <88189374+igor399@users.noreply.github.com>
2022-12-07 11:44:30 +0300

    REST

---
+++ src/main/java/by/ch/spring/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package by.ch.spring.service;
+
+import by.ch.spring.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
